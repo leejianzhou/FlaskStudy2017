7ac129c6b9026dd9379db03315cf6a0c4e02d24f
leejianzhou <leejianzhou@163.com>
2017-10-13 13:17:02 +0800

    Dynamic routes (2b)

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!' % name
+    return '<h1>Hello, %s!!!!' % name
 
 if __name__ == '__main__':
     app.run(debug=True)

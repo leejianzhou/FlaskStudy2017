bca69ecc415690ee1c598b0d41131f3f4b30e015
leejianzhou <leejianzhou@gmail.com>
2017-10-16 15:15:41 +0800

    3C

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+
 #from flask.ext.script import Manager
 #from flask_script import Manager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 #manager = Manager(app)
 
 @app.route('/')

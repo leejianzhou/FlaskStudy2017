0695b93edb2d09164fa368f8b4d25acae9609a2f
leejianzhou <leejianzhou@163.com>
2017-10-17 21:44:22 +0800

    Flask-Moment（3e）

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 #from flask.ext.script import Manager
 #from flask_script import Manager
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 #manager = Manager(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):

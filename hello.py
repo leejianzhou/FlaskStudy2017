4b0104183273f7db60f65045b381dfc0c7daa805
leejianzhou <leejianzhou@163.com>
2017-10-19 20:41:24 +0800

    Redirect(4b)

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -25,13 +25,12 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
 
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):

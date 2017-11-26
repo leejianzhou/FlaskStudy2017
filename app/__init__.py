2547242f5e601a4c73404721c6aaed1799e4a135
leejianzhou <leejianzhou@gmail.com>
2017-11-26 21:44:20 +0800

    20171126

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,21 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
-from config import config
+##from config import config
+from ..config import config
 
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -22,8 +29,12 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    login_manager.init_app(app)
+
+    #from .main import main as main_blueprint
+    #app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
